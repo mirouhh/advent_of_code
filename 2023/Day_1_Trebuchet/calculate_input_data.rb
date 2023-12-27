feb1677301dadf2c309ffd936cd8e640da9c5eec
Björn Jensen <mirouhh@googlemail.com>
2023-12-27 18:48:12 +0100

    [Day 01/23] Improve readability of test output as my solution is not recognizing everything correctly

--- 2023/Day_1_Trebuchet/calculate_input_data.rb
+++ 2023/Day_1_Trebuchet/calculate_input_data.rb
@@ -1,11 +1,13 @@
 require_relative 'lib/aoc23/value_calculator'
 
 value_calculator = ValueCalculator.new
-
+lines_found = 0
 File.readlines(File.dirname(__FILE__) + '/input.txt').each do |line|
-  puts "Adding #{line}"
+  puts "Adding #{line.chomp}, value: #{line.int_array.value}"
   value_calculator.add_value(line)
+  lines_found += 1
 end
 
 puts '#' * 80
+puts "Lines found: #{lines_found}"
 puts "The total sum of the lines in the input file is #{value_calculator.sum}"

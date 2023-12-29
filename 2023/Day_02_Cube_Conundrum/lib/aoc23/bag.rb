2495ca02125fc50906948af746827e0b718948c0
Björn Jensen <mirouhh@googlemail.com>
2023-12-29 15:25:28 +0100

    [Day 02/23] Adding missing import functionality in order to make it easy to initialize a bag with a given set of data

--- 2023/Day_02_Cube_Conundrum/lib/aoc23/bag.rb
+++ 2023/Day_02_Cube_Conundrum/lib/aoc23/bag.rb
@@ -17,8 +17,20 @@ class Bag
     (contains? cube_color) ? @cubes[cube_color] = (@cubes[cube_color] + 1) : @cubes[cube_color] = 1
   end
 
-  def count_cubes(cube_color)
+  def how_many(cube_color)
     cube_color = cube_color.downcase
     (contains? cube_color) ? @cubes[cube_color] : 0
+  end
+
+  def import(data)
+    data = data.chomp.downcase
+    amount_search_pattern = /\d/i
+    color_search_pattern = /[a-z]/i
+    partial_data = data.split(',')
+    partial_data.each do | data |
+      color = data.scan(color_search_pattern).join
+      amount = data.scan(amount_search_pattern).join.to_i
+      amount.times { add_cube(color) }
+    end
   end
 end
--- 2023/Day_02_Cube_Conundrum/test/aoc23/bag_test.rb
+++ 2023/Day_02_Cube_Conundrum/test/aoc23/bag_test.rb
@@ -2,29 +2,35 @@ require 'minitest/autorun'
 require_relative '../../lib/aoc23/bag'
 
 class BagTest < Minitest::Test
-  def test_new_bag_is_empty
-    bag = Bag.new
 
-    assert bag.empty?
+  def setup
+    @bag = Bag.new
+  end
+  def test_new_bag_is_empty
+    assert @bag.empty?
   end
 
   def test_can_add_cubes
-    bag = Bag.new
-    bag.add_cube('blue')
+    @bag.add_cube('blue')
 
-    assert bag.contains? 'blue'
+    assert @bag.contains? 'blue'
   end
 
   def test_can_count_amount_of_blue_cubes
-    bag = Bag.new
-    3.times { bag.add_cube('blue') }
+    3.times { @bag.add_cube('blue') }
 
-    assert_equal 3, bag.count_cubes('blue')
+    assert_equal 3, @bag.how_many('blue')
   end
 
   def test_return_0_if_bag_does_not_contain_cube
-    bag = Bag.new
+    assert_equal 0, @bag.how_many('blue')
+  end
 
-    assert_equal 0, bag.count_cubes('blue')
+  def test_import_data
+    assert @bag.empty?
+    @bag.import('12 red, 13 green, 14 blue')
+    assert_equal 12, @bag.how_many('red')
+    assert_equal 13, @bag.how_many('green')
+    assert_equal 14, @bag.how_many('blue')
   end
 end

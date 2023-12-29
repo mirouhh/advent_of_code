b5447e5828bf93bab607e878b888aa8436a7f611
Björn Jensen <mirouhh@googlemail.com>
2023-12-29 14:41:50 +0100

    [Day 02/23] Adding functionality to create a new game from import data

--- 2023/Day_02_Cube_Conundrum/lib/aoc23/game.rb
+++ 2023/Day_02_Cube_Conundrum/lib/aoc23/game.rb
@@ -22,4 +22,36 @@ class Game
     end
     info + set_infos.join('; ')
   end
+
+  def add_cube_sets(cube_sets)
+    cube_sets.each do |cube_set|
+      add_cube_set(cube_set)
+    end
+  end
+
+  def self.create(data)
+    data = data.chomp.downcase
+    id = Game.get_id_from_import_data(data)
+    game = Game.new(id)
+    cube_sets = Game.get_cube_sets_from_import_data(data)
+    game.add_cube_sets(cube_sets)
+    game
+  end
+
+  def self.get_id_from_import_data(import_data)
+    id_search_pattern = /\d/i
+    partial_data = import_data.split(':')
+    partial_data[0].scan(id_search_pattern).join.to_i
+  end
+
+  def self.get_cube_sets_from_import_data(import_data)
+    cube_sets = []
+    cube_sets_data = import_data.split(':')[1].split(';')
+    cube_sets_data.each do |cube_set_data|
+      cube_set = CubeSet.new
+      cube_set.import(cube_set_data)
+      cube_sets << cube_set
+    end
+    cube_sets
+  end
 end
--- 2023/Day_02_Cube_Conundrum/test/aoc23/game_test.rb
+++ 2023/Day_02_Cube_Conundrum/test/aoc23/game_test.rb
@@ -3,35 +3,29 @@ require_relative '../../lib/aoc23/game'
 require_relative '../../lib/aoc23/cube_set'
 
 class GameTest < Minitest::Test
-  def test_has_id
-    game = Game.new(1)
 
-    assert_equal 1, game.id
+  def setup
+    @game = Game.new(1)
+    @set_1 = CubeSet.new
+    @set_1.add_cubes('blue', 3)
+    @set_1.add_cubes('red', 4)
+  end
+  def test_has_id
+    assert_equal 1, @game.id
   end
 
   def test_has_empty_cube_sets
-    game = Game.new(1)
-
-    assert game.empty?
+    assert @game.empty?
   end
 
   def test_has_cube_set
-    set = CubeSet.new
-    set.add_cubes('blue', 3)
-    set.add_cubes('red', 4)
+    @game.add_cube_set(@set_1)
 
-    game = Game.new(1)
-    game.add_cube_set(set)
-
-    assert_equal false, game.empty?
-    assert_equal 'Game 1: 3 blue, 4 red', game.info
+    assert_equal false, @game.empty?
+    assert_equal 'Game 1: 3 blue, 4 red', @game.info
   end
 
   def test_has_cube_sets
-    set_1 = CubeSet.new
-    set_1.add_cubes('blue', 3)
-    set_1.add_cubes('red', 4)
-
     set_2 = CubeSet.new
     set_2.add_cubes('red', 1)
     set_2.add_cubes('green', 2)
@@ -40,13 +34,36 @@ class GameTest < Minitest::Test
     set_3 = CubeSet.new
     set_3.add_cubes('green', 2)
 
-    game = Game.new(1)
-    game.add_cube_set(set_1)
-    game.add_cube_set(set_2)
-    game.add_cube_set(set_3)
+    @game.add_cube_set(@set_1)
+    @game.add_cube_set(set_2)
+    @game.add_cube_set(set_3)
 
-    assert_equal false, game.empty?
-    assert_equal 'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green', game.info
+    assert_equal false, @game.empty?
+    assert_equal 'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green', @game.info
+  end
+
+  def test_can_add_multiple_cube_sets
+    set_2 = CubeSet.new
+    set_2.add_cubes('red', 1)
+    set_2.add_cubes('green', 2)
+    set_2.add_cubes('blue', 6)
+
+    set_3 = CubeSet.new
+    set_3.add_cubes('green', 2)
+
+    cube_sets = []
+    cube_sets << @set_1
+    cube_sets << set_2
+    cube_sets << set_3
+
+    @game.add_cube_sets(cube_sets)
+    assert_equal false, @game.empty?
+    assert_equal 'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green', @game.info
+  end
+  def test_create_game_from_string
+    created_game = Game.create('Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red')
+
+    assert_equal 'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red', created_game.info
   end
 
 end

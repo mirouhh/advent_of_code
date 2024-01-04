3d2ed3fd953225c8c14bd56e1f51024a0b018815
Björn Jensen <mirouhh@googlemail.com>
2024-01-04 23:00:08 +0100

    [Dey 03/23] Refactoring test code in order to improve readability

--- 2023/Day_03_Gear_Ratios/test/aoc23/part_number_detector_test.rb
+++ 2023/Day_03_Gear_Ratios/test/aoc23/part_number_detector_test.rb
@@ -2,17 +2,19 @@ require 'minitest/autorun'
 require_relative '../../lib/aoc23/part_number_detector'
 
 class PartNumberDetectorTest < Minitest::Test
-  def test_has_no_valid_part_numbers
-    part_number_detector = PartNumberDetector.new
-    part_number_detector.import("467..114..")
 
-    assert_equal false, part_number_detector.has_valid_part_numbers?
+  def setup
+    @part_number_detector = PartNumberDetector.new
+  end
+  def test_has_no_valid_part_numbers
+    @part_number_detector.import("467..114..")
+
+    assert_equal false, @part_number_detector.has_valid_part_numbers?
   end
 
   def test_has_a_symbol
-    part_number_detector = PartNumberDetector.new
-    part_number_detector.import('...*......')
+    @part_number_detector.import('...*......')
 
-    assert part_number_detector.has_symbols?
+    assert @part_number_detector.has_symbols?
   end
 end

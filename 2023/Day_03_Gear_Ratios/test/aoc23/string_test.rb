687129df74275c92a64050299256c858316e6c8b
Björn Jensen <mirouhh@googlemail.com>
2024-01-06 20:04:29 +0100

    [Day 03/23] Adding test to check if symbols are detected

--- 2023/Day_03_Gear_Ratios/test/aoc23/string_test.rb
+++ 2023/Day_03_Gear_Ratios/test/aoc23/string_test.rb
@@ -3,8 +3,12 @@ require_relative '../../lib/aoc23/string'
 
 class StringTest < Minitest::Test
 
-  def test_has_no_symbols_when_no_symbols_contained
+  def test_has_no_symbols_when_no_symbols_are_provided
     assert ''.symbols.empty?
+  end
+
+  def test_has_symbols_when_symbols_are_provided
+    assert '*'.symbols.any?
   end
 
 end

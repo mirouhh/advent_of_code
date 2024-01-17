d4681d95ebe9905a8df3efc4eaccdcbe1d0ba6e6
Björn Jensen <mirouhh@googlemail.com>
2024-01-17 23:07:17 +0100

    [Day 03/23] Validating that a symbol could occur multiple times in a string

--- 2023/Day_03_Gear_Ratios/test/aoc23/string_test.rb
+++ 2023/Day_03_Gear_Ratios/test/aoc23/string_test.rb
@@ -20,16 +20,16 @@ class StringTest < Minitest::Test
   def test_has_only_one_symbol_regardless_how_often_a_symbol_is_provided
     test_string = '******'
     assert test_string.symbols.include? '*'
-    assert_equal 1, test_string.symbols.length
+    assert_equal 6, test_string.symbols.length
   end
 
   def test_the_delimiter_is_not_a_symbol
-    test_string = '...*......'
+    test_string = '...*...*..'
     delimiter = '.'
 
     assert test_string.symbols.include? '*'
     assert test_string.symbols.none? delimiter
-    assert_equal 1, test_string.symbols.length
+    assert_equal 2, test_string.symbols.length
   end
 
   def test_has_two_symbols

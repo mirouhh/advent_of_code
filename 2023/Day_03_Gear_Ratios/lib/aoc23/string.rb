51c84cdddf58d507eea2d4a7450403f527598334
Björn Jensen <mirouhh@googlemail.com>
2024-01-23 22:54:26 +0100

    [Day 03/23] Fix: Indexes are correct

--- 2023/Day_03_Gear_Ratios/lib/aoc23/string.rb
+++ 2023/Day_03_Gear_Ratios/lib/aoc23/string.rb
@@ -13,11 +13,22 @@ class String
     indexes = []
     start_index = 0
 
+    search_pattern = /[^0-9a-z.]/i
+    search_pattern = /\d+/ if substring.match?(/[^0-9a-z.]/i)
+
+    b1 = true
+    b2 = true
+
     while start_index = index(substring, start_index)
       end_index = start_index + substring.length
-      indexes << start_index
+      if substring.match?(/[0-9]/)
+        b1 = self[start_index - 1].match?(/[^0-9]/i) unless self[start_index - 1].nil?
+        b2 = self[start_index + substring.length].match?(/[^0-9]/i) unless self[start_index + substring.length].nil?
+      end
+      indexes << start_index if (b1 && b2)
       start_index = end_index
     end
     indexes
   end
+
 end

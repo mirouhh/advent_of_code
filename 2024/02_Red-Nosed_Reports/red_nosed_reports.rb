2b1f8b6e768ecc6392f76acb9d29b4e97c8bb3e5
Björn Jensen <mirouhh@googlemail.com>
2024-12-06 23:36:44 +0100

    [Red-Nosed Reports] Extracting missing safe reports

--- 2024/02_Red-Nosed_Reports/red_nosed_reports.rb
+++ 2024/02_Red-Nosed_Reports/red_nosed_reports.rb
@@ -1,18 +1,27 @@
 require_relative 'lib/report'
 require_relative 'lib/problem_dampener'
 
-safe_reports = 0
-reports = Array.new()
+safe_reports = []
+reports = []
 
 File.readlines("#{File.dirname(__FILE__)}/input.txt", chomp: true).each do |line|
   reports << line.split(' ')
   report = Report.new(line)
   if report.safe?
-    safe_reports += 1
+    safe_reports << report.content
   else
     ProblemDampener.import(report)
-    safe_reports += 1 if ProblemDampener.tolerable?
+    safe_reports << report.content if ProblemDampener.tolerable?
   end
 end
 
-puts "Safe reports: #{safe_reports}"
+puts "Safe reports: #{safe_reports.count}"
+
+File.readlines("#{File.dirname(__FILE__)}/safe.txt", chomp: true).each do |line|
+  safe_report = Report.new(line)
+  safe_reports.include?(safe_report.content) ? safe_reports.delete(safe_report.content) : safe_reports << safe_report.content
+end
+
+puts "Missing reports: #{safe_reports.count}"
+
+safe_reports.each { | safe_report | puts safe_report.join(' ') }

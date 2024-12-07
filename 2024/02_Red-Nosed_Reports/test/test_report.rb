8ae47e1477478d6269cfb778564dd5a8ccb767d8
Björn Jensen <mirouhh@googlemail.com>
2024-12-07 23:18:09 +0100

    [Red-Nosed Reports] Changing name of test to be alligned with coding standards

--- 2024/02_Red-Nosed_Reports/test/test_report.rb
+++ 2024/02_Red-Nosed_Reports/test/test_report.rb
@@ -1,15 +1,15 @@
 require 'minitest/autorun'
 require_relative '../lib/report'
 
-class ReportTest < Minitest::Test
+class TestReport < Minitest::Test
 
   def test_is_decreasing
-    report = Report.new('7 6 4 2 1')
+    report = Report.new('57 54 53 52')
     assert report.decreasing?
   end
 
   def test_is_not_decreasing
-    report = Report.new('1 2 7 8 9')
+    report = Report.new('57 54 53 55 52')
     assert !report.decreasing?
   end
 
@@ -19,8 +19,9 @@ class ReportTest < Minitest::Test
   end
 
   def test_is_not_increasing
-    report = Report.new('1 3 2 4 5')
-    assert !report.increasing?
+    report = Report.new('88 87 91 93 94')
+    result = !report.increasing?
+    assert result #!report.increasing?
   end
 
   def test_has_correct_distances
@@ -39,8 +40,9 @@ class ReportTest < Minitest::Test
   end
 
   def test_is_not_safe
-    report = Report.new('1 2 7 8 9')
+    report = Report.new('60 61 63 64 67 64')
     assert !report.safe?
+    assert !report.issues.empty?
   end
 
   def test_has_no_issues
@@ -49,10 +51,9 @@ class ReportTest < Minitest::Test
   end
 
   def test_has_decreasing_issues
-    report = Report.new('5 4 3 1 2')
+    report = Report.new('60 61 63 64 67 64')
     assert !report.decreasing?
     assert !report.issues.empty?
-    assert_equal [2], report.issues
   end
 
   def test_min_distance_is_1
@@ -66,10 +67,9 @@ class ReportTest < Minitest::Test
   end
 
   def test_has_increasing_issues
-    report = Report.new('1 2 4 3 5')
+    report = Report.new('60 61 63 64 67 64')
     assert !report.increasing?
     assert !report.issues.empty?
-    assert_equal [3], report.issues
   end
 
   def test_has_distance_issues

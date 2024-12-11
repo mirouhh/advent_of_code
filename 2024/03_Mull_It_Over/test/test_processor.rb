52068a487b5a43731282aec96e6a746dbef50839
Björn Jensen <mirouhh@googlemail.com>
2024-12-11 21:45:49 +0100

    [Mull It Over] Writing tests to check if the implementation could handle different instructions

--- 2024/03_Mull_It_Over/test/test_processor.rb
+++ 2024/03_Mull_It_Over/test/test_processor.rb
@@ -1,11 +1,33 @@
 require 'minitest/autorun'
+require_relative '../lib/processor'
 
 class TestProcessor < Minitest::Test
 
-  def test_scan
+  def test_scan_mul_2_by_4
     input = "mul(2,4)"
     Processor.scan(input)
-    assert !Process.instructions.empty?
+    assert !Processor.instructions.empty?
+    assert_equal ["mul(2,4)"], Processor.instructions
+  end
+
+  def test_scan_mul_5_by_5
+    input = "mul(5,5)"
+    Processor.scan(input)
+    assert !Processor.instructions.empty?
+    assert_equal ["mul(5,5)"], Processor.instructions
+  end
+
+  def test_scan_test_data
+    input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+    Processor.scan(input)
+    assert !Processor.instructions.empty?
+    assert_equal ["mul(2,4)", "mul(5,5)", "mul(11,8)", "mul(8,5)"], Processor.instructions
+  end
+
+  def test_process_test_data
+    input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+    Processor.scan(input)
+    assert_equal 161, Processor.process()
   end
 
 end

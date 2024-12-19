1bf130ba5c77ac0a90c221a43bf881e2a0f3b400
Björn Jensen <mirouhh@googlemail.com>
2024-12-19 22:16:41 +0100

    [Ceres Search] Adding tests to ensure that the handling of multiple lines work as well

--- 2024/04_Ceres_Search/test/test_processor.rb
+++ 2024/04_Ceres_Search/test/test_processor.rb
@@ -26,4 +26,34 @@ class TestProcessor < Minitest::Test
     assert_empty processor.data
   end
 
+  def test_read_multiple_line_data
+    line_1 = 'NWX&/1'
+    line_2 = 'OSAMXT'
+    line_3 = 'IAJFAD'
+    line_4 = 'XMASVS'
+    line_5 = 'YXCWRL'
+    processor = Processor.new()
+    processor.read(line_1)
+    processor.read(line_2)
+    processor.read(line_3)
+    processor.read(line_4)
+    processor.read(line_5)
+    assert_equal [line_1.chars, line_2.chars, line_3.chars, line_4.chars, line_5.chars], processor.data
+  end
+
+  def test_clean_up_multiple_line_data
+    line_1 = 'NWX&/1'
+    line_2 = 'OSAMXT'
+    line_3 = 'IAJFAD'
+    line_4 = 'XMASVS'
+    line_5 = 'YXCWRL'
+    processor = Processor.new()
+    processor.read(line_1)
+    processor.read(line_2)
+    processor.read(line_3)
+    processor.read(line_4)
+    processor.read(line_5)
+    processor.clean_up_data
+    assert_equal ['..X...'.chars, '.SAMX.'.chars, '.A..A.'.chars, 'XMAS.S'.chars, '.X....'.chars], processor.data
+  end
 end

66178512b8ee16540b7fedb1f8f365246b3ed6a6
Björn Jensen <mirouhh@googlemail.com>
2025-12-09 20:56:05 +0100

    [Secret Entrence : Part 01] test: current position should be 50

--- 2025/Day_01_Secret_Entrance/secret_entrance.rb
+++ 2025/Day_01_Secret_Entrance/secret_entrance.rb
@@ -1,4 +1,5 @@
 start_position = 50
 
 ## Tests ##
-puts start_position == 50 ? "✓ Test 1 bestanden" : "✗ Test 1 FEHLGESCHLAGEN: erwartet 0, bekommen #{start_position}"
+puts start_position == 50 ? "✓ Test 1 passed" : "✗ Test 1 FAILED: expected 50, was #{start_position}"
+puts current_position == 50 ? "✓ Test 2 passed" : "✗ Test 1 FAILED: expected 50, was #{start_position}"

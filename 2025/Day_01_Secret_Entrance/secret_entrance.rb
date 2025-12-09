8703bc440bdfa53558bcc413b403e55894785abf
Björn Jensen <mirouhh@googlemail.com>
2025-12-09 21:06:57 +0100

    [Secret Entrance : Part 01] feat: current position is 49 after rotating 1 step

--- 2025/Day_01_Secret_Entrance/secret_entrance.rb
+++ 2025/Day_01_Secret_Entrance/secret_entrance.rb
@@ -1,8 +1,9 @@
 start_position = 50
 current_position = start_position
-current_position -= 1
 
-## Tests ##
 puts start_position == 50 ? "✓ Test 1 passed" : "✗ Test 1 FAILED: expected 50, was #{start_position}"
 puts current_position == 50 ? "✓ Test 2 passed" : "✗ Test 2 FAILED: expected 50, was #{start_position}"
+
+current_position -= 1
+
 puts current_position == 49 ? "✓ Test 3 passed" : "✗ Test 3 FAILED: expected 49, was #{start_position}"

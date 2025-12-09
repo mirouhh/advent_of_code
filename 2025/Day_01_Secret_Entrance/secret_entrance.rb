7b48511258a35c4f35b6f6e739488748b4ca536e
Björn Jensen <mirouhh@googlemail.com>
2025-12-09 21:33:00 +0100

    [Secret Entrance : Part 01] refactor: implementing a more generic rotate functionality

--- 2025/Day_01_Secret_Entrance/secret_entrance.rb
+++ 2025/Day_01_Secret_Entrance/secret_entrance.rb
@@ -1,12 +1,13 @@
 start_position = 50
 @current_position = start_position
 
-def rotate_left(distance)
-  @current_position -= distance
-end
-
-def rotate_right(distance)
-  @current_position += distance
+def rotate(direction, steps)
+  case direction
+  when "L"
+    @current_position -= steps
+  when "R"
+    @current_position += steps
+  end
 end
 
 ## Test correct start position
@@ -16,20 +17,21 @@ puts start_position == 50 ? "✓ Test 1 passed" : "✗ Test 1 FAILED: expected 5
 puts @current_position == 50 ? "✓ Test 2 passed" : "✗ Test 2 FAILED: expected 50, was #{@current_position}"
 
 ## Test that current position equals is updated correctly when rotating 1 step to the left
-rotate_left(1)
+rotate("L", 1)
 
 puts @current_position == 49 ? "✓ Test 3 passed" : "✗ Test 3 FAILED: expected 49, was #{@current_position}"
 
 ## Test that current position equals is updated correctly when rotating 1 step to the right
-rotate_right(1)
+rotate("R", 1)
 
 puts @current_position == 50 ? "✓ Test 4 passed" : "✗ Test 4 FAILED: expected 50, was #{@current_position}"
 
 ## Test that current position equals is updated correctly when rotating 13 steps to the left
-rotate_left(13)
+rotate("L", 13)
 
 puts @current_position == 37 ? "✓ Test 5 passed" : "✗ Test 5 FAILED: expected 37, was #{@current_position}"
 
 ## Test that current position equals is updated correctly when rotating 40 steps to the right
-rotate_right(40)
+rotate("R", 40)
+
 puts @current_position == 77 ? "✓ Test 6 passed" : "✗ Test 6 FAILED: expected 77, was #{@current_position}"

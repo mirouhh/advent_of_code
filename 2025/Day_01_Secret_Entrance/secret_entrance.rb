9f71eb16441662a30f66a815dd943bca47fc32d7
Björn Jensen <mirouhh@googlemail.com>
2025-12-09 21:22:07 +0100

    [Secret Entrance : Part 01] test: rotating more steps to the left should point to the correct position

--- 2025/Day_01_Secret_Entrance/secret_entrance.rb
+++ 2025/Day_01_Secret_Entrance/secret_entrance.rb
@@ -9,13 +9,21 @@ def rotate_right(distance)
   @current_position += distance
 end
 
+## Test correct start position
 puts start_position == 50 ? "✓ Test 1 passed" : "✗ Test 1 FAILED: expected 50, was #{start_position}"
+
+## Test that current position equals start position in the very beginning
 puts @current_position == 50 ? "✓ Test 2 passed" : "✗ Test 2 FAILED: expected 50, was #{@current_position}"
 
+## Test that current position equals is updated correctly when rotating 1 step to the left
 rotate_left(1)
 
 puts @current_position == 49 ? "✓ Test 3 passed" : "✗ Test 3 FAILED: expected 49, was #{@current_position}"
 
+## Test that current position equals is updated correctly when rotating 1 step to the right
 rotate_right(1)
 
 puts @current_position == 50 ? "✓ Test 4 passed" : "✗ Test 4 FAILED: expected 50, was #{@current_position}"
+
+## Test that current position equals is updated correctly when rotating 13 steps to the left
+puts @current_position == 37 ? "✓ Test 5 passed" : "✗ Test 5 FAILED: expected 37, was #{@current_position}"

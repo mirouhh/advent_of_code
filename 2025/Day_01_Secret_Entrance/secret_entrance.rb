6c2b0b5d800e47a3911a109584350fa6293eef36
Björn Jensen <mirouhh@googlemail.com>
2025-12-09 21:01:01 +0100

    [Secret Entrance : Part 01] test: current position should be 49
     After rotating one step to the left the current position must be updated

--- 2025/Day_01_Secret_Entrance/secret_entrance.rb
+++ 2025/Day_01_Secret_Entrance/secret_entrance.rb
@@ -3,4 +3,5 @@ current_position = start_position
 
 ## Tests ##
 puts start_position == 50 ? "✓ Test 1 passed" : "✗ Test 1 FAILED: expected 50, was #{start_position}"
-puts current_position == 50 ? "✓ Test 2 passed" : "✗ Test 1 FAILED: expected 50, was #{start_position}"
+puts current_position == 50 ? "✓ Test 2 passed" : "✗ Test 2 FAILED: expected 50, was #{start_position}"
+puts current_position == 49 ? "✓ Test 3 passed" : "✗ Test 3 FAILED: expected 49, was #{start_position}"

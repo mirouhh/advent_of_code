77946310682784d148346e1468712cfb848a3f8f
Björn Jensen <mirouhh@googlemail.com>
2025-12-09 21:42:30 +0100

    [Secret Entrance : Part 01] feat: implementing parsing of instruction and executing it

--- 2025/Day_01_Secret_Entrance/secret_entrance.rb
+++ 2025/Day_01_Secret_Entrance/secret_entrance.rb
@@ -1,6 +1,12 @@
 start_position = 50
 @current_position = start_position
 
+def dial(instruction)
+  direction = instruction[0]
+  steps = instruction[1..instruction.length].to_i
+  rotate(direction, steps)
+end
+
 def rotate(direction, steps)
   case direction
   when "L"

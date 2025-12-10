c601f8d7e37fbe851eac42f9430af04ab57bfaed
Björn Jensen <mirouhh@googlemail.com>
2025-12-10 20:36:23 +0100

    [Secret Entrance : Part 01] refactor: keeping the main file for program execution

--- 2025/Day_01_Secret_Entrance/secret_entrance.rb
+++ 2025/Day_01_Secret_Entrance/secret_entrance.rb
@@ -1,17 +1,1 @@
-@start_position = 50
-@current_position = @start_position
 
-def dial(instruction)
-  direction = instruction[0]
-  steps = instruction[1..instruction.length].to_i
-  rotate(direction, steps)
-end
-
-def rotate(direction, steps)
-  case direction
-  when "L"
-    @current_position -= steps
-  when "R"
-    @current_position += steps
-  end
-end

4e00a24bcd53b3538c682d856e8a6acfcb008a1f
Björn Jensen <mirouhh@googlemail.com>
2025-12-10 22:50:02 +0100

    [Secret Entrance : Part 02]  test: SafeDial should detect the correct total amount of zeros for puzzle data

--- 2025/Day_01_Secret_Entrance/secret_entrance.rb
+++ 2025/Day_01_Secret_Entrance/secret_entrance.rb
@@ -3,4 +3,4 @@ require_relative 'lib/safe_dial'
 safeDial = SafeDial.new
 safeDial.instructions = File.readlines("#{File.dirname(__FILE__)}/input.txt", chomp: true)
 safeDial.dial
-puts "#{safeDial.zero_count} zeros counted!"
+puts "#{safeDial.zeros} zeros counted!"

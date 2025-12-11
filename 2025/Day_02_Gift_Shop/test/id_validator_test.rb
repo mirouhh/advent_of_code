ffe15a9b14edbdd518e8d7811c4e7ec57e08998d
Björn Jensen <mirouhh@googlemail.com>
2025-12-11 15:52:34 +0100

    [Gift Shop : Part 01] test: ID Validator should be able to import a given range

--- 2025/Day_02_Gift_Shop/test/id_validator_test.rb
+++ 2025/Day_02_Gift_Shop/test/id_validator_test.rb
@@ -2,4 +2,9 @@ require 'minitest/autorun'
 require_relative '../lib/id_validator'
 
 class IDValidatorTest < Minitest::Test
+
+  def test_import_range_works
+    IDValidator.import("11-22")
+    assert IDValidator.range.wont_be_empty
+  end
 end

aac9cb05512c91075bac2f17d9a919d1d41c6a57
Björn Jensen <mirouhh@googlemail.com>
2025-12-12 21:04:52 +0100

    [Gift Shop : Part 01] feat: main program calculates sample data correctly

--- 2025/Day_02_Gift_Shop/gift_shop.rb
+++ 2025/Day_02_Gift_Shop/gift_shop.rb
@@ -1,7 +1,10 @@
 require_relative 'lib/id_validator'
 
+sum = 0
 ranges = File.readlines("#{File.dirname(__FILE__)}/sample_input.txt", chomp: true)[0].split(",")
 ranges.each do |range|
   IDValidator.validate(range)
-  puts "Invalid IDs in range #{range}: #{IDValidator.invalid_ids.to_s}"
+  puts "Invalid IDs in range #{range}: #{IDValidator.invalid_ids.to_s}" unless IDValidator.invalid_ids.empty?
+  sum += IDValidator.sum unless IDValidator.invalid_ids.empty?
 end
+puts "All invalid IDs are sumed up to: #{sum}"

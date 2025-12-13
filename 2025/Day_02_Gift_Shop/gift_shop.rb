6625f4831f8705d58f599eda99144495b0fa9b94
Björn Jensen <mirouhh@googlemail.com>
2025-12-13 19:51:43 +0100

    [Gift Shop : Part 02] test: ID Validator should work for sample data for new constraints as well

--- 2025/Day_02_Gift_Shop/gift_shop.rb
+++ 2025/Day_02_Gift_Shop/gift_shop.rb
@@ -1,21 +1,33 @@
 require_relative 'lib/id_validator'
 
 sum = 0
+puts "####### PART ONE ###############################"
 puts "#### Validating and calculating sample data ####"
 ranges = File.readlines("#{File.dirname(__FILE__)}/sample_input.txt", chomp: true)[0].split(",")
 ranges.each do |range|
   IDValidator.validate(range)
-  puts "Invalid IDs in range #{range}: #{IDValidator.invalid_ids.to_s}" unless IDValidator.invalid_ids.empty?
+  puts "Invalid IDs in range #{range}: #{IDValidator.invalid_ids}" unless IDValidator.invalid_ids.empty?
   sum += IDValidator.sum unless IDValidator.invalid_ids.empty?
 end
-puts "All invalid IDs are sumed up to: #{sum}\n\n"
+puts "All invalid IDs are summed up to: #{sum}\n\n"
 
 sum = 0
 puts "#### Validating and calculating puzzle data ####"
 ranges = File.readlines("#{File.dirname(__FILE__)}/input.txt", chomp: true)[0].split(",")
 ranges.each do |range|
   IDValidator.validate(range)
-  puts "Invalid IDs in range #{range}: #{IDValidator.invalid_ids.to_s}" unless IDValidator.invalid_ids.empty?
+  puts "Invalid IDs in range #{range}: #{IDValidator.invalid_ids}" unless IDValidator.invalid_ids.empty?
   sum += IDValidator.sum unless IDValidator.invalid_ids.empty?
 end
-puts "All invalid IDs are sumed up to: #{sum}\n\n"
+puts "All invalid IDs are summed up to: #{sum}\n\n"
+
+sum = 0
+puts "####### PART TWO ###############################"
+puts "#### Validating and calculating sample data ####"
+ranges = File.readlines("#{File.dirname(__FILE__)}/sample_input.txt", chomp: true)[0].split(",")
+ranges.each do |range|
+  IDValidator.validate_part_two(range)
+  puts "Invalid IDs in range #{range}: #{IDValidator.invalid_ids_part_two}" unless IDValidator.invalid_ids_part_two.empty?
+  sum += IDValidator.sum_part_two unless IDValidator.invalid_ids_part_two.empty?
+end
+puts "All invalid IDs are summed up to: #{sum}\n\n"

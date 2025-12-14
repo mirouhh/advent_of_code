2805210fd98f66e48d3dfac4be664c0e0e93c10a
Björn Jensen <mirouhh@googlemail.com>
2025-12-14 17:56:16 +0100

    [Lobby : Part 01] test: a new battery bank could be set up with batteries

--- 2025/Day_03_Lobby /test/battery_bank_test.rb
+++ 2025/Day_03_Lobby /test/battery_bank_test.rb
@@ -8,4 +8,10 @@ class BatteryBankTest < Minitest::Test
     assert battery_bank.empty?
   end
 
+  def test_battery_bank_with_batteries
+    battery_bank = BatteryBank.new("12345")
+    assert_equal false, battery_bank.empty?
+    assert_equal "12345", battery_bank.batteries
+  end
+
 end

6549194b3f5d1cb911d0e274911c87ff6c731fd9
Björn Jensen <mirouhh@googlemail.com>
2025-12-14 19:30:56 +0100

    [Lobby : Part 01] feat: a battery bank can identify the two most powerful batteries

--- 2025/Day_03_Lobby /lib/battery_bank.rb
+++ 2025/Day_03_Lobby /lib/battery_bank.rb
@@ -9,8 +9,16 @@ class BatteryBank
     @batteries.empty?
   end
 
-  def max_index
-    @batteries.index(@batteries.max)
+  def max_index(amount=1)
+    return @batteries.index(@batteries.max) if amount == 1
+    indices = []
+    max = @batteries[0..(@batteries.length-2)].max
+    max_index = @batteries.index(max)
+    indices << max_index
+    second_part_of_batteries = @batteries[(max_index+1)..-1]
+    second_max = second_part_of_batteries.max
+    second_max_index = max_index + 1 + second_part_of_batteries.index(second_max)
+    indices << second_max_index
   end
 
   def max_voltage

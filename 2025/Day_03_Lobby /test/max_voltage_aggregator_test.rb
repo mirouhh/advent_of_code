0e4299aceeea20702629249b5f9c7834c27aaf2b
Björn Jensen <mirouhh@googlemail.com>
2025-12-14 16:50:23 +0100

    [Lobby : Part 01] test: MaxVoltageAggregator should be able to import a battery bank

--- 2025/Day_03_Lobby /test/max_voltage_aggregator_test.rb
+++ 2025/Day_03_Lobby /test/max_voltage_aggregator_test.rb
@@ -3,4 +3,10 @@ require_relative '../lib/max_voltage_aggregator'
 
 class MaxVoltageAggregatorTest < Minitest::Test
 
+  def test_import_battery_bank
+    battery_bank = "12345"
+    MaxVoltageAggregator.import_battery()
+    assert_equal false, MaxVoltageAggregator.battery_banks.empty?
+  end
+
 end

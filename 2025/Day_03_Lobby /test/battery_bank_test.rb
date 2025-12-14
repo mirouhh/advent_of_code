ae2ee4475915895def583fbe9ae051ab276408d9
Björn Jensen <mirouhh@googlemail.com>
2025-12-14 17:52:30 +0100

    [Lobby : Part 01] test: a new battery banks should be empty

--- 2025/Day_03_Lobby /test/battery_bank_test.rb
+++ 2025/Day_03_Lobby /test/battery_bank_test.rb
@@ -1,5 +1,11 @@
 require 'minitest/autorun'
+require_relative '../lib/battery_bank'
 
 class BatteryBankTest < Minitest::Test
-  
+
+  def test_battery_bank_is_empty
+    battery_bank = BatteryBank.new
+    assert battery_bank.empty?
+  end
+
 end

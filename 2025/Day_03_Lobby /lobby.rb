1c6b54952f54d8e50894ace21546d6ef81330d0f
Björn Jensen <mirouhh@googlemail.com>
2025-12-15 07:27:29 +0100

    [Lobby : Part 01] test: puzzle data should be aggregatedd correctly

--- 2025/Day_03_Lobby /lobby.rb
+++ 2025/Day_03_Lobby /lobby.rb
@@ -1,10 +1,22 @@
 require_relative 'lib/battery_bank'
 require_relative 'lib/max_voltage_aggregator'
 
-sum = 0
-puts "####### PART ONE ###############################"
+puts "####### PART ONE    ###############################"
+puts "####### Sample Data ###############################"
 MaxVoltageAggregator.reset
 File.readlines("#{File.dirname(__FILE__)}/sample_input.txt", chomp: true).each do |line|
+  puts "Importing #{line}"
+  battery_bank = BatteryBank.new(line)
+  battery_bank.locate_max(2)
+  MaxVoltageAggregator.import_battery_bank(battery_bank)
+end
+
+puts "The total maximum voltage produced by the sample data is #{MaxVoltageAggregator.sum}"
+
+puts "####### PART ONE    ###############################"
+puts "####### Puzzle Data ###############################"
+MaxVoltageAggregator.reset
+File.readlines("#{File.dirname(__FILE__)}/input.txt", chomp: true).each do |line|
   puts "Importing #{line}"
   battery_bank = BatteryBank.new(line)
   battery_bank.locate_max(2)

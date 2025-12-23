d5aa73bbc789d0d166a1cf03473340896c6ec02d
Björn Jensen <mirouhh@googlemail.com>
2025-12-23 12:02:32 +0100

    [Printing Department : Part 01] test: grid should be clearable

--- 2025/Day_04_Printing_Department/test/grid_test.rb
+++ 2025/Day_04_Printing_Department/test/grid_test.rb
@@ -3,27 +3,33 @@ require_relative '../lib/grid'
 
 class GridTest < Minitest::Test
 
+  def setup
+    @grid = Grid.new
+  end
+
   def test_grid_is_empty
-    grid = Grid.new
-    assert grid.empty?
+    assert @grid.empty?
   end
 
   def test_grid_can_add_shelf
-    grid = Grid.new
-    grid.add('..@@.@@@@.')
-    assert_equal false, grid.empty?
+    @grid.add('..@@.@@@@.')
+    assert_equal false, @grid.empty?
   end
 
   def test_shelf_includes_paper_roll
-    grid = Grid.new
-    grid.add('..@@.@@@@.')
-    assert grid.paper_roll?(0)
+    @grid.add('..@@.@@@@.')
+    assert @grid.paper_roll?(0)
   end
 
   def test_shelf_includes_no_paper_roll
-    grid = Grid.new
-    grid.add('..........')
-    assert_equal false, grid.paper_roll?(0)
+    @grid.add('..........')
+    assert_equal false, @grid.paper_roll?(0)
+  end
+
+  def test_grid_could_be_cleared
+    @grid.add('..@@.@@@@.')
+    @grid.clear
+    assert @grid.empty?
   end
 
 end

4bdebfa86993dcffb757029e34daea1ba3a8a4bd
Björn Jensen <mirouhh@googlemail.com>
2025-12-23 12:49:12 +0100

    [Printing Department : Part 01] feat: grid finds adjacent paper rolls on a shelf

--- 2025/Day_04_Printing_Department/lib/grid.rb
+++ 2025/Day_04_Printing_Department/lib/grid.rb
@@ -26,4 +26,15 @@ class Grid
     @shelfs[shelf].chars.each_with_index.select { |char, index| char == '@' }.map(&:last)
   end
 
+  def adjacent_paper_rolls(paper_roll)
+    shelf_no = paper_roll[0]
+    position = paper_roll[1]
+
+    adjacent_paper_rolls = []
+    shelf_content = @shelfs[shelf_no].chars[position - 1]
+    adjacent_paper_rolls << shelf_content if shelf_content == '@'
+
+    shelf_content = @shelfs[shelf_no].chars[position + 1]
+    adjacent_paper_rolls << shelf_content if shelf_content == '@'
+  end
 end

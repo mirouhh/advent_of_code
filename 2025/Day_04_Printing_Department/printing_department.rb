083ca983f809310cbb7f0671e32c1c46685c9997
Björn Jensen <mirouhh@googlemail.com>
2025-12-27 16:12:37 +0100

    [Printing Department : Part 01] test: adding program to handle puzzle input

--- 2025/Day_04_Printing_Department/printing_department.rb
+++ 2025/Day_04_Printing_Department/printing_department.rb
@@ -9,4 +9,17 @@ File.readlines("#{File.dirname(__FILE__)}/sample_input.txt", chomp: true).each d
 end
 
 puts "\n"
-puts "The grid includes #{grid.accessable_paper_rolls.size} accessable paper rolls"
+puts "The sample grid includes #{grid.accessable_paper_rolls.size} accessable paper rolls"
+
+puts "\n\n"
+puts "####### PART ONE    ###############################"
+puts "####### Puzzle Data ###############################"
+
+grid.clear
+File.readlines("#{File.dirname(__FILE__)}/input.txt", chomp: true).each do |shelf|
+  puts "Importing shelf #{shelf}"
+  grid.add(shelf)
+end
+
+puts "\n"
+puts "The puzzle grid includes #{grid.accessable_paper_rolls.size} accessable paper rolls"

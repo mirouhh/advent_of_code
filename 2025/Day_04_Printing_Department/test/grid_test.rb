104958342260931845563f4ad60026ee5f781caa
Björn Jensen <mirouhh@googlemail.com>
2025-12-27 11:58:27 +0100

    [Printing Department : Part 01] test: grid should be able to find one adjacent paper roll on the shelf below

--- 2025/Day_04_Printing_Department/test/grid_test.rb
+++ 2025/Day_04_Printing_Department/test/grid_test.rb
@@ -86,4 +86,12 @@ class GridTest < Minitest::Test
     assert_equal 3, adjacent_paper_rolls.size
   end
 
+  def test_find_one_adjacent_paper_roll_on_shelf_below
+    @grid.add('...@....')
+    @grid.add('..@.....')
+    paper_roll = [1, 3]
+    adjacent_paper_rolls = @grid.adjacent_paper_rolls(paper_roll)
+    assert_equal 1, adjacent_paper_rolls.size
+  end
+
 end

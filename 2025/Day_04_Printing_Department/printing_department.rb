395d87f855f46383bb5bba9d03ee111c510a5775
Björn Jensen <mirouhh@googlemail.com>
2025-12-28 11:02:55 +0100

    [Printing Department : Part 02] test: program to handle puzzle input for part two

--- 2025/Day_04_Printing_Department/printing_department.rb
+++ 2025/Day_04_Printing_Department/printing_department.rb
@@ -36,3 +36,16 @@ grid.pick_up_all_accessable_paper_rolls
 
 puts "\n"
 puts "#{grid.picked_up_paper_rolls} paper roll(s) in total can be removed by the Elves and their forklifts"
+
+puts "\n\n"
+puts "####### PART TWO    ###############################"
+puts "####### Puzzle Data ###############################"
+grid.clear
+File.readlines("#{File.dirname(__FILE__)}/input.txt", chomp: true).each do |shelf|
+  puts "Importing shelf #{shelf}"
+  grid.add(shelf)
+end
+grid.pick_up_all_accessable_paper_rolls
+
+puts "\n"
+puts "#{grid.picked_up_paper_rolls} paper roll(s) in total can be removed by the Elves and their forklifts"

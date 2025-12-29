3de7c99c1034b598a78305845a27a5a3c41a70bf
Björn Jensen <mirouhh@googlemail.com>
2025-12-29 15:56:25 +0100

    [Cafeteria : Part 01] test: an ingredient validator should start with an empty list of ids

--- 2025/Day_05_Cafeteria/test/ingredient_validator_test.rb
+++ 2025/Day_05_Cafeteria/test/ingredient_validator_test.rb
@@ -3,4 +3,9 @@ require_relative '../lib/ingredient_validator'
 
 class IngredientValidatorTest < Minitest::Test
 
+  def test_has_empty_id_range
+    ingredient_validator = IngredientValidator.new
+    assert ingredient_validator.id_ranges.empty?
+  end
+
 end

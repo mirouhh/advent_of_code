e00258d832dea1f450ffdc41e02bc8abde668f1f
Björn Jensen <mirouhh@googlemail.com>
2025-12-29 16:10:59 +0100

    [Cafeteria : Part 01]  reafactor: utilizing the setup method to make the test code more readable

--- 2025/Day_05_Cafeteria/test/ingredient_validator_test.rb
+++ 2025/Day_05_Cafeteria/test/ingredient_validator_test.rb
@@ -3,21 +3,22 @@ require_relative '../lib/ingredient_validator'
 
 class IngredientValidatorTest < Minitest::Test
 
+  def setup
+    @ingredient_validator = IngredientValidator.new
+  end
+
   def test_has_empty_id_range
-    ingredient_validator = IngredientValidator.new
-    assert ingredient_validator.id_ranges.empty?
+    assert @ingredient_validator.id_ranges.empty?
   end
 
   def test_stores_an_id_range
-    ingredient_validator = IngredientValidator.new
-    ingredient_validator.add_id_range(1..3)
-    assert_equal 1..3, ingredient_validator.id_ranges[0]
+    @ingredient_validator.add_id_range(1..3)
+    assert_equal 1..3, @ingredient_validator.id_ranges[0]
   end
 
   def test_validate_freshness
-    ingredient_validator = IngredientValidator.new
-    ingredient_validator.add_id_range(1..3)
-    assert ingredient_validator.fresh?(2)
+    @ingredient_validator.add_id_range(1..3)
+    assert @ingredient_validator.fresh?(2)
   end
 
 end

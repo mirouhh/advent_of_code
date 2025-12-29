9c555aa61809ddca9e60f74f5a7fc45c38e68c23
Björn Jensen <mirouhh@googlemail.com>
2025-12-29 16:41:02 +0100

    [Cafeteria : Part 01] test: the input format of a range is given as a string

--- 2025/Day_05_Cafeteria/test/ingredient_validator_test.rb
+++ 2025/Day_05_Cafeteria/test/ingredient_validator_test.rb
@@ -12,28 +12,28 @@ class IngredientValidatorTest < Minitest::Test
   end
 
   def test_stores_an_id_range
-    @ingredient_validator.add_id_range(1..3)
+    @ingredient_validator.add_id_range('1-3')
     assert_equal 1..3, @ingredient_validator.id_ranges[0]
   end
 
   def test_validate_freshness
-    @ingredient_validator.add_id_range(1..3)
+    @ingredient_validator.add_id_range('1-3')
     assert @ingredient_validator.fresh?(2)
   end
 
   def test_validate_spoiledness
-    @ingredient_validator.add_id_range(1..3)
+    @ingredient_validator.add_id_range('1-3')
     assert_equal false, @ingredient_validator.fresh?(4)
   end
 
   def test_validate_freshness_of_multiple_ingredients
-    @ingredient_validator.add_id_range(3..5)
+    @ingredient_validator.add_id_range('3-5')
     assert @ingredient_validator.fresh?(3, 4, 5)
   end
 
   def test_validate_freshness_of_multiple_ingredients_in_multiple_id_ranges
-    @ingredient_validator.add_id_range(3..5)
-    @ingredient_validator.add_id_range(10..14)
+    @ingredient_validator.add_id_range('3-5')
+    @ingredient_validator.add_id_range('10-14')
     assert @ingredient_validator.fresh?(5, 11)
   end
 

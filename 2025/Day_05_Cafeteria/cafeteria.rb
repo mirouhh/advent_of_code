455a5b30244a100bd1b08b133eed7ae39422831e
Björn Jensen <mirouhh@googlemail.com>
2025-12-29 18:17:41 +0100

    [Cafeteria : Part 01] test: program to handle puzzle input for part one

--- 2025/Day_05_Cafeteria/cafeteria.rb
+++ 2025/Day_05_Cafeteria/cafeteria.rb
@@ -9,7 +9,30 @@ File.readlines("#{File.dirname(__FILE__)}/sample_input.txt", chomp: true).each d
     puts "\n"
     next
   end
-  
+
+  if line.include?('-')
+    puts "Importing range '#{line}'"
+    ingredient_validator.add_id_range(line)
+  else
+    ingredient_validator.fresh?(line) ? puts("#{line} is fresh") : puts("#{line} is not fresh")
+  end
+end
+
+puts "\n"
+puts "#{ingredient_validator.fresh_ingredients_amount} of the available ingredient IDs are fresh"
+
+ingredient_validator.clear
+
+puts "\n\n"
+puts "####### PART ONE ###############################"
+puts "#### Importing and validating puzzle data ####"
+
+File.readlines("#{File.dirname(__FILE__)}/input.txt", chomp: true).each do |line|
+  if line.empty?
+    puts "\n"
+    next
+  end
+
   if line.include?('-')
     puts "Importing range '#{line}'"
     ingredient_validator.add_id_range(line)

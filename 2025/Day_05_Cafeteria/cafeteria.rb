0354126ed2e0d63c1d5dab6cc0f1f7b15f8c0a5d
Björn Jensen <mirouhh@googlemail.com>
2025-12-29 18:10:04 +0100

    [Cafeteria : Part 01] test: program to handle sample input for part two

---
+++ 2025/Day_05_Cafeteria/cafeteria.rb
@@ -0,0 +1,22 @@
+require_relative 'lib/ingredient_validator'
+
+puts "####### PART ONE ###############################"
+puts "#### Importing and validating sample data ####"
+
+ingredient_validator = IngredientValidator.new
+File.readlines("#{File.dirname(__FILE__)}/sample_input.txt", chomp: true).each do |line|
+  if line.empty?
+    puts "\n"
+    next
+  end
+  
+  if line.include?('-')
+    puts "Importing range '#{line}'"
+    ingredient_validator.add_id_range(line)
+  else
+    ingredient_validator.fresh?(line) ? puts("#{line} is fresh") : puts("#{line} is not fresh")
+  end
+end
+
+puts "\n"
+puts "#{ingredient_validator.fresh_ingredients_amount} of the available ingredient IDs are fresh"

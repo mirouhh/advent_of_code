a7ad8d0d9930b053957e07bfa47e5f9f782169a0
Björn Jensen <mirouhh@googlemail.com>
2025-12-30 15:48:55 +0100

    [Trash Compactor : Part 01] test: Calculator should be able to handle + with a single operand

--- 2025/Day_06_Trash_Compactor/test/cephalopod_calculator_test.rb
+++ 2025/Day_06_Trash_Compactor/test/cephalopod_calculator_test.rb
@@ -3,4 +3,9 @@ require_relative '../lib/cephalopod_calculator'
 
 class CephalopodCalculatorTest < Minitest::Test
 
+  def test_sum_with_single_operand
+    cephalopodCalculator = CephalopodCalculator.new
+    assert_equal 1, cephalopodCalculator.calculate('1', '+')
+  end
+
 end

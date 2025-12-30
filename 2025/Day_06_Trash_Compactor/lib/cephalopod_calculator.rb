df3ab371820e641d2fd39132c21376cc512546bb
Björn Jensen <mirouhh@googlemail.com>
2025-12-30 16:13:53 +0100

    [Trash Compactor : Part 01] feat: Calculator can calculate the grand total of all calculations so far

--- 2025/Day_06_Trash_Compactor/lib/cephalopod_calculator.rb
+++ 2025/Day_06_Trash_Compactor/lib/cephalopod_calculator.rb
@@ -1,6 +1,13 @@
 class CephalopodCalculator
 
+  def initialize
+    @totals = []
+  end
   def calculate(*operands, operator)
-    operands.map(&:to_i).reduce(operator.to_sym)
+    operands.map(&:to_i).reduce(operator.to_sym).tap { | total | @totals << total }
+  end
+
+  def grand_total
+    @totals.sum
   end
 end

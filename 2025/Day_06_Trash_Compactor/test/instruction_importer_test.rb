a0aac07350b7bb66bf1ffe6f3d4675d3c11d2745
Björn Jensen <mirouhh@googlemail.com>
2025-12-31 16:21:58 +0100

    [Trash Compactor : Part 02] refactor: make test environment more readable

--- 2025/Day_06_Trash_Compactor/test/instruction_importer_test.rb
+++ 2025/Day_06_Trash_Compactor/test/instruction_importer_test.rb
@@ -3,19 +3,19 @@ require_relative '../lib/instruction_importer'
 
 class InstructionImporterTest < Minitest::Test
 
-  def test_import_instructions_from_file
-    instruction_importer = InstructionImporter.new
+  def setup
+    @instruction_importer = InstructionImporter.new
     file = "#{File.dirname(__FILE__)}/../sample_input.txt"
-    instruction_importer.import(file)
-    assert_equal false, instruction_importer.data.empty?
+    @instruction_importer.import(file)
+  end
+
+  def test_import_instructions_from_file
+    assert_equal false, @instruction_importer.data.empty?
   end
 
   def test_imported_data_can_be_reset
-    instruction_importer = InstructionImporter.new
-    file = "#{File.dirname(__FILE__)}/../sample_input.txt"
-    instruction_importer.import(file)
-    instruction_importer.reset
-    assert instruction_importer.data.empty?
+    @instruction_importer.reset
+    assert @instruction_importer.data.empty?
   end
 
 end

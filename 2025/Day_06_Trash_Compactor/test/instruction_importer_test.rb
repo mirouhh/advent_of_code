be4458fe7b8a784b237b5807ef01f565969fc64e
Björn Jensen <mirouhh@googlemail.com>
2025-12-31 16:06:16 +0100

    [Trash Compactor : Part 02] test: importer should be able to import data from a file

--- 2025/Day_06_Trash_Compactor/test/instruction_importer_test.rb
+++ 2025/Day_06_Trash_Compactor/test/instruction_importer_test.rb
@@ -3,4 +3,10 @@ require_relative '../lib/instruction_importer'
 
 class InstructionImporterTest < Minitest::Test
 
+  def test_import_instructions_from_file
+    instruction_importer = InstructionImporter.new
+    instruction_importer.import('sample_input.txt')
+    assert_equal false, instruction_importer.data.empty?
+  end
+
 end

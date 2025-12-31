bfa1c5b72371061e13a85d9eb4848c59bdbd53e9
Björn Jensen <mirouhh@googlemail.com>
2025-12-31 16:16:15 +0100

    [Trash Compactor : Part 02] test: in order to make importer reusable the imported data should be able to be reset

--- 2025/Day_06_Trash_Compactor/test/instruction_importer_test.rb
+++ 2025/Day_06_Trash_Compactor/test/instruction_importer_test.rb
@@ -10,4 +10,12 @@ class InstructionImporterTest < Minitest::Test
     assert_equal false, instruction_importer.data.empty?
   end
 
+  def test_imported_data_can_be_reset
+    instruction_importer = InstructionImporter.new
+    file = "#{File.dirname(__FILE__)}/../sample_input.txt"
+    instruction_importer.import(file)
+    instruction_importer.reset
+    assert instruction_importer.data.empty?
+  end
+
 end

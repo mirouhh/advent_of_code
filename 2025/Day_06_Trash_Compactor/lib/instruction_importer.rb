72630775e2ffe47a1fb46e5473ac4688d8207bb3
Björn Jensen <mirouhh@googlemail.com>
2025-12-31 16:47:24 +0100

    [Trash Compactor : Part 02] feat: importer provides instructions for puzzle part one

--- 2025/Day_06_Trash_Compactor/lib/instruction_importer.rb
+++ 2025/Day_06_Trash_Compactor/lib/instruction_importer.rb
@@ -1,17 +1,25 @@
 class InstructionImporter
 
-  attr_reader :data
+  attr_reader :data, :instructions_part_one
 
   def initialize()
     @data = []
+    @instructions_part_one = []
   end
 
   def import(file)
     @data = File.readlines("#{file}", chomp: true).reject(&:empty?)
+    extract_instructions_part_one
   end
 
   def reset
     @data.clear
+    @instructions_part_one.clear
+  end
+
+  private
+  def extract_instructions_part_one
+    @instructions_part_one =@data[0..-2].map(&:split).transpose.zip(@data[-1].split).map(&:flatten)
   end
 
 end

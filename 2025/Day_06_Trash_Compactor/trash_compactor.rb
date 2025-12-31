1bc4a10e4011c542962a0f90ab8b1c7e532171a5
Björn Jensen <mirouhh@googlemail.com>
2025-12-31 18:09:00 +0100

    [Trash Compactor : Part 02] test: program handles puzzle input for part two

--- 2025/Day_06_Trash_Compactor/trash_compactor.rb
+++ 2025/Day_06_Trash_Compactor/trash_compactor.rb
@@ -47,3 +47,17 @@ instructionImporter.instructions_part_two.each do |instruction |
 end
 
 puts "The grand total of the sample data is #{cephalopodCalculator.grand_total}."
+
+puts "\n\n"
+puts "#### Importing and validating puzzle data ####"
+
+instructionImporter.reset
+cephalopodCalculator.reset
+
+instructionImporter.import(puzzle_file)
+
+instructionImporter.instructions_part_two.each do |instruction |
+  puts "#{instruction[0..-2].join(' ' + instruction[-1] + ' ')} = #{cephalopodCalculator.execute_instruction(instruction)}"
+end
+
+puts "The grand total of the puzzle data is #{cephalopodCalculator.grand_total}."

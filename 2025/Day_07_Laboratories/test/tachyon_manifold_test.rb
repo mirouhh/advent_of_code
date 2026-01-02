fb913df61146c474efcd35eb91825dd44907e0f8
Björn Jensen <mirouhh@googlemail.com>
2026-01-02 23:30:20 +0100

    [Laboratories : Part 01] test: tachyon manifolds should know how a diagram starts

--- 2025/Day_07_Laboratories/test/tachyon_manifold_test.rb
+++ 2025/Day_07_Laboratories/test/tachyon_manifold_test.rb
@@ -3,4 +3,9 @@ require_relative '../lib/tachyon_manifold'
 
 class TachyonManifoldTest < Minitest::Test
 
+  def test_start_symbol
+    tachyonManifold = TachyonManifold.new
+    assert_equal 'S', tachyonManifold.start_symbol
+  end
+
 end

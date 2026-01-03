f6dd3cbe7110dcb766809d0cdc6d6f8ddea9a676
Björn Jensen <mirouhh@googlemail.com>
2026-01-03 13:59:53 +0100

    [Laboratories : Part 01] test: program should to handle the puzzle diagram for part one

--- 2025/Day_07_Laboratories/laboratories.rb
+++ 2025/Day_07_Laboratories/laboratories.rb
@@ -8,6 +8,7 @@ tachyonManifold.splitter = '^'
 tachyonManifold.beam = '|'
 
 sample_diagram = "#{File.dirname(__FILE__)}/sample_input.txt"
+puzzle_diagram = "#{File.dirname(__FILE__)}/input.txt"
 
 puts "####### PART ONE ###############################"
 puts "#### Importing sample diagram ##################"
@@ -19,3 +20,13 @@ puts tachyonManifold.to_s
 puts "\n"
 
 puts "The sample diagram contains #{tachyonManifold.total_splits} splits.\n\n"
+
+tachyonManifold.reset_diagram
+
+tachyonManifold.import(File.readlines("#{puzzle_diagram}", chomp: true).reject(&:empty?))
+
+puts "\n"
+puts tachyonManifold.to_s
+puts "\n"
+
+puts "The puzzle diagram contains #{tachyonManifold.total_splits} splits.\n\n"

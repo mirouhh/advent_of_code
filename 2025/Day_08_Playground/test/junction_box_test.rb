a28faa15d5d222d0df7ae65a587944f42ed36a0d
Björn Jensen <mirouhh@googlemail.com>
2026-01-04 18:59:58 +0100

    [Playground : Part 01] refactor: simplifying test code of junction box

--- 2025/Day_08_Playground/test/junction_box_test.rb
+++ 2025/Day_08_Playground/test/junction_box_test.rb
@@ -3,33 +3,34 @@ require_relative '../lib/junction_box'
 
 class JunctionBoxTest < Minitest::Test
 
+  def setup
+    @junction_box = JunctionBox.new
+  end
+
   def test_new_junction_box_doesnt_have_nil_values
-    junction_box = JunctionBox.new(1,2,3)
-    refute_nil junction_box.x
-    refute_nil junction_box.y
-    refute_nil junction_box.z
+    refute_nil @junction_box.x
+    refute_nil @junction_box.y
+    refute_nil @junction_box.z
   end
 
   def test_default_values_are_zero
-    junction_box = JunctionBox.new
-    assert_equal 0, junction_box.x
-    assert_equal 0, junction_box.y
-    assert_equal 0, junction_box.z
+    assert_equal 0, @junction_box.x
+    assert_equal 0, @junction_box.y
+    assert_equal 0, @junction_box.z
   end
 
   def test_junction_box_has_correct_values
-    junction_box = JunctionBox.new(1,2,3)
+    junction_box = JunctionBox.new(1, 2, 3)
     assert_equal 1, junction_box.x
     assert_equal 2, junction_box.y
     assert_equal 3, junction_box.z
   end
 
   def test_new_position
-    junction_box = JunctionBox.new
-    junction_box.position(5, 6, 7)
-    assert_equal 5, junction_box.x
-    assert_equal 6, junction_box.y
-    assert_equal 7, junction_box.z
+    @junction_box.position(5, 6, 7)
+    assert_equal 5, @junction_box.x
+    assert_equal 6, @junction_box.y
+    assert_equal 7, @junction_box.z
   end
 
 end

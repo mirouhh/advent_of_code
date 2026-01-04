41fe6f27e5d2a8c6cffe30e1891fb9c2b5be7121
Björn Jensen <mirouhh@googlemail.com>
2026-01-04 19:00:51 +0100

    [Playground : Part 01] test: a new circuit calculator should be without any circuits

--- 2025/Day_08_Playground/test/circuit_calculator_test.rb
+++ 2025/Day_08_Playground/test/circuit_calculator_test.rb
@@ -3,4 +3,9 @@ require_relative '../lib/circuit_calculator'
 
 class CircuitCalculatorTest < Minitest::Test
 
+  def test_circuit_calculator_is_empty
+    circuit_calculator = CircuitCalculator.new
+    assert circuit_calculator.empty?
+  end
+
 end

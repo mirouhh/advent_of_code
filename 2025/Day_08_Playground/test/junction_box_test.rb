e78f6912cab7815deba7ad57a83c25289801a182
Björn Jensen <mirouhh@googlemail.com>
2026-01-04 14:39:44 +0100

    [Playground : Part 01] fix: using refute to check if something is not nil

--- 2025/Day_08_Playground/test/junction_box_test.rb
+++ 2025/Day_08_Playground/test/junction_box_test.rb
@@ -5,9 +5,9 @@ class JunctionBoxTest < Minitest::Test
 
   def test_new_junction_box_doesnt_have_nil_values
     junction_box = JunctionBox.new(1,2,3)
-    assert_not_nil junction_box.x
-    assert_not_nil junction_box.y
-    assert_not_nil junction_box.z
+    refute_nil junction_box.x
+    refute_nil junction_box.y
+    refute_nil junction_box.z
   end
 
 end

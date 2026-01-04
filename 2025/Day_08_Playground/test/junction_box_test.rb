dcb7a374b23b81087749e81597558fda80380eda
Björn Jensen <mirouhh@googlemail.com>
2026-01-04 14:43:51 +0100

    [Playground : Part 01] test: a new junction box should be created with correct values for each dimension

--- 2025/Day_08_Playground/test/junction_box_test.rb
+++ 2025/Day_08_Playground/test/junction_box_test.rb
@@ -17,4 +17,11 @@ class JunctionBoxTest < Minitest::Test
     assert_equal 0, junction_box.z
   end
 
+  def test_junction_box_has_correct_values
+    junction_box = JunctionBox.new(1,2,3)
+    assert_equal 1, junction_box.x
+    assert_equal 2, junction_box.y
+    assert_equal 3, junction_box.z
+  end
+
 end

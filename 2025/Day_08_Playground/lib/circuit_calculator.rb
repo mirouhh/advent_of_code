c6c09ecb5ab9bd74c7bc62596e2a9eb11328f469
Björn Jensen <mirouhh@googlemail.com>
2026-01-06 08:44:53 +0100

    [Playground : Part 01] feat: circuit calculator calculates the distance between two junction boxes

--- 2025/Day_08_Playground/lib/circuit_calculator.rb
+++ 2025/Day_08_Playground/lib/circuit_calculator.rb
@@ -14,4 +14,7 @@ class CircuitCalculator
     @circuits << junction_box
   end
 
+  def distance(junction_box_one, junction_box_two)
+    Math.sqrt((junction_box_one.x - junction_box_two.x)**2 + (junction_box_one.y - junction_box_two.y)**2 + (junction_box_one.z - junction_box_two.z)**2)
+  end
 end

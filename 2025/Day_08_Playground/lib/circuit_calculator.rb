58095dea80797b548ae7c1e9bb6a1371ed407252
Björn Jensen <mirouhh@googlemail.com>
2026-01-06 09:01:35 +0100

    [Playground : Part 01] feat: a circuit calculator knows distances between junction boxes

--- 2025/Day_08_Playground/lib/circuit_calculator.rb
+++ 2025/Day_08_Playground/lib/circuit_calculator.rb
@@ -1,9 +1,11 @@
 class CircuitCalculator
 
-  attr_reader :circuits
+  attr_reader :circuits, :distances
 
   def initialize
     @circuits = []
+    @distances = []
+    @junction_boxes = []
   end
 
   def empty?
@@ -11,10 +13,18 @@ class CircuitCalculator
   end
 
   def add(junction_box)
+    update_distances(junction_box)
     @circuits << junction_box
+    @junction_boxes << junction_box
   end
 
   def distance(junction_box_one, junction_box_two)
     Math.sqrt((junction_box_one.x - junction_box_two.x)**2 + (junction_box_one.y - junction_box_two.y)**2 + (junction_box_one.z - junction_box_two.z)**2)
+  end
+
+  private
+
+  def update_distances(new_junction_box)
+    @junction_boxes.each { |junction_box| @distances << distance(new_junction_box, junction_box) }
   end
 end

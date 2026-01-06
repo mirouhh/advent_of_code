437ca8c213a2e8380f3091ef16ab0200678a7858
Björn Jensen <mirouhh@googlemail.com>
2026-01-06 08:23:06 +0100

    [Playground : Part 01] refactor: making the tests more readable

--- 2025/Day_08_Playground/test/circuit_calculator_test.rb
+++ 2025/Day_08_Playground/test/circuit_calculator_test.rb
@@ -4,18 +4,20 @@ require_relative '../lib/junction_box'
 
 class CircuitCalculatorTest < Minitest::Test
 
+  def setup
+    @circuit_calculator = CircuitCalculator.new
+  end
+
   def test_circuit_calculator_is_empty
-    circuit_calculator = CircuitCalculator.new
-    assert circuit_calculator.empty?
+    assert @circuit_calculator.empty?
   end
 
   def test_circuit_can_add_junction_boxes
-    circuit_calculator = CircuitCalculator.new
     junction_box_one = JunctionBox.new(162,817,812)
     junction_box_two = JunctionBox.new(57,618,57)
-    circuit_calculator.add(junction_box_one)
-    circuit_calculator.add(junction_box_two)
-    assert_equal 2, circuit_calculator.circuits.size
+    @circuit_calculator.add(junction_box_one)
+    @circuit_calculator.add(junction_box_two)
+    assert_equal 2, @circuit_calculator.circuits.size
   end
 
 end

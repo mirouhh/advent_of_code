720ea9c56d7f57ce722c31ffd54e7a054c0af119
Björn Jensen <mirouhh@googlemail.com>
2026-01-06 08:16:35 +0100

    [Playground : Part 01] test: a circuit calculator should be able to add junction boxes

--- 2025/Day_08_Playground/test/circuit_calculator_test.rb
+++ 2025/Day_08_Playground/test/circuit_calculator_test.rb
@@ -1,11 +1,21 @@
 require 'minitest/autorun'
 require_relative '../lib/circuit_calculator'
+require_relative '../lib/junction_box'
 
 class CircuitCalculatorTest < Minitest::Test
 
   def test_circuit_calculator_is_empty
     circuit_calculator = CircuitCalculator.new
     assert circuit_calculator.empty?
+  end
+
+  def test_circuit_can_add_junction_boxes
+    circuit_calculator = CircuitCalculator.new
+    junction_box_one = JunctionBox.new(162,817,812)
+    junction_box_two = JunctionBox.new(57,618,57)
+    circuit_calculator.add(junction_box_one)
+    circuit_calculator.add(junction_box_two)
+    assert_equal 2, circuit_calculator.circuits.size
   end
 
 end

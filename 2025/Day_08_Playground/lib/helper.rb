0600af952ffa569c5f6fd119c72675506e5f1f9d
Björn Jensen <mirouhh@googlemail.com>
2026-01-10 20:10:08 +0100

    [Playground : Part 02] test: program should to handle sample data for part two

--- 2025/Day_08_Playground/lib/helper.rb
+++ 2025/Day_08_Playground/lib/helper.rb
@@ -1,7 +1,7 @@
 JunctionBox = Data.define(:x, :y, :z)
 Distance = Data.define(:value, :start, :end)
 
-def calculation_info(circuit_calculator, connection_amount, top_circuit_size)
+def calculation_info_part_one(circuit_calculator, connection_amount, top_circuit_size)
   circuit_calculator.connect_boxes(connection_amount)
   sizes = circuit_calculator.circuits.first(top_circuit_size).map(&:size).join(', ')
   product = circuit_calculator.multiply(top_circuit_size)
@@ -13,4 +13,13 @@ def calculation_info(circuit_calculator, connection_amount, top_circuit_size)
     The largest circuits have the sizes #{sizes} which leads to #{product} as a result.
 
   INFO
+end
+
+def calculation_info_part_two(circuit_calculator)
+  circuit_calculator.connect_all
+  start_x = circuit_calculator.last_connection.start.x
+  end_x = circuit_calculator.last_connection.end.x
+  product = circuit_calculator.last_connection_product
+
+  "Multiplying the X coordinates of last two junction boxes (#{start_x} and #{end_x}) produces #{product}.\n\n"
 end
--- 2025/Day_08_Playground/playground.rb
+++ 2025/Day_08_Playground/playground.rb
@@ -4,15 +4,17 @@ require_relative 'lib/circuit_calculator'
 sample_data = "#{File.dirname(__FILE__)}/sample_input.txt"
 puzzle_data = "#{File.dirname(__FILE__)}/input.txt"
 
-puts "####### PART ONE ###############################"
-puts "#### Importing sample data #####################"
+puts "####### PART ONE & TWO ###############################"
+puts "#### Importing sample data ###########################"
 puts "\n"
 circuit_calculator = CircuitCalculator.new
 circuit_calculator.import(sample_data)
-puts calculation_info(circuit_calculator, 10, 3)
+puts calculation_info_part_one(circuit_calculator, 10, 3)
+
+puts calculation_info_part_two(circuit_calculator)
 
 puts "#### Importing puzzle data ######################"
 puts "\n"
 circuit_calculator.clear
 circuit_calculator.import(puzzle_data)
-puts calculation_info(circuit_calculator, 1000, 3)
+puts calculation_info_part_one(circuit_calculator, 1000, 3)

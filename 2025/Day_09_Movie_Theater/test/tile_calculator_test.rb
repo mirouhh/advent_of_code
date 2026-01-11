448376367cdab136b7de79726ddd6ef76b51c0eb
Björn Jensen <mirouhh@googlemail.com>
2026-01-11 17:56:06 +0100

    [Movie Theater : Part 01] test: a tile should be able to add red tiles

--- 2025/Day_09_Movie_Theater/test/tile_calculator_test.rb
+++ 2025/Day_09_Movie_Theater/test/tile_calculator_test.rb
@@ -5,6 +5,12 @@ class TileCalculatorTest < Minitest::Test
 
   def test_new_calculator_is_empty
     tile_calculator = TileCalculator.new
-    assert tile_calculator.empty?
+    assert_empty tile_calculator
+  end
+
+  def test_calculator_can_add_red_tiles
+    tile_calculator = TileCalculator.new
+    tile_calculator.add_red_tile(1,2)
+    refute_empty tile_calculator
   end
 end

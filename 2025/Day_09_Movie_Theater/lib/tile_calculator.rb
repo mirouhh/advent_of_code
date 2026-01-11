0bbf199ede7c78cb0d2e36e14581b419a97c6eb4
Björn Jensen <mirouhh@googlemail.com>
2026-01-11 18:11:53 +0100

    [Movie Theater : Part 01] feat: a tile calculator can import data from a file

--- 2025/Day_09_Movie_Theater/lib/tile_calculator.rb
+++ 2025/Day_09_Movie_Theater/lib/tile_calculator.rb
@@ -17,4 +17,11 @@ class TileCalculator
   def clean
     @red_tiles.clear
   end
+
+  def import(file)
+    File.readlines("#{file}", chomp: true).reject(&:empty?).each do |line |
+      x, y = line.split(',').map(&:to_i)
+      add_red_tile(x, y)
+    end
+  end
 end

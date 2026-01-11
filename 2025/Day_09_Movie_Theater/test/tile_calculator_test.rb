218a3a586838dc803dcfcd0a103850f593e34d4c
Björn Jensen <mirouhh@googlemail.com>
2026-01-11 18:16:29 +0100

    [Movie Theater : Part 01] refactor: cleaning up the tests

--- 2025/Day_09_Movie_Theater/test/tile_calculator_test.rb
+++ 2025/Day_09_Movie_Theater/test/tile_calculator_test.rb
@@ -3,31 +3,31 @@ require_relative '../lib/tile_calculator'
 
 class TileCalculatorTest < Minitest::Test
 
+  def setup
+    @tile_calculator = TileCalculator.new
+  end
+
   def test_new_calculator_is_empty
-    tile_calculator = TileCalculator.new
-    assert tile_calculator.empty?
+    assert_empty @tile_calculator
   end
 
   def test_calculator_can_add_red_tiles
-    tile_calculator = TileCalculator.new
-    tile_calculator.add_red_tile(1,2)
-    refute_empty tile_calculator
+    @tile_calculator.add_red_tile(1,2)
+    refute_empty @tile_calculator
   end
 
   def test_calculator_can_be_cleaned
-    tile_calculator = TileCalculator.new
-    assert_empty tile_calculator
-    tile_calculator.add_red_tile(1,2)
-    refute_empty tile_calculator
-    tile_calculator.clean
-    assert tile_calculator
+    assert_empty @tile_calculator
+    @tile_calculator.add_red_tile(1,2)
+    refute_empty @tile_calculator
+    @tile_calculator.clean
+    assert_empty @tile_calculator
   end
 
   def test_calculator_can_import_data_from_file
     file = "#{File.dirname(__FILE__)}/../sample_input.txt"
-    tile_calculator = TileCalculator.new
-    assert_empty tile_calculator
-    tile_calculator.import(file)
-    refute_empty tile_calculator
+    assert_empty @tile_calculator
+    @tile_calculator.import(file)
+    refute_empty @tile_calculator
   end
 end

075666101c517b4c9c72fcc475d4c3b2d89db08b
Björn Jensen <mirouhh@googlemail.com>
2026-01-11 17:51:56 +0100

    [Movie Theater : Part 01] test: a new tile calculator should be empty

--- 2025/Day_09_Movie_Theater/test/tile_calculator_test.rb
+++ 2025/Day_09_Movie_Theater/test/tile_calculator_test.rb
@@ -1,4 +1,9 @@
 require 'minitest/autorun'
 
 class TileCalculatorTest < Minitest::Test
+
+  def test_new_calculator_is_empty
+    tile_calculator = TileCalculator.new
+    assert tile_calculator.empty?
+  end
 end

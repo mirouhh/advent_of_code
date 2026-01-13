8c44d62f18c229e9c3e7e8499012d2b32b088f57
Björn Jensen <mirouhh@googlemail.com>
2026-01-14 00:45:43 +0100

    [Movie Theater : Part 02] test: program should to handle puzzle data for part two

--- 2025/Day_09_Movie_Theater/movie_theater.rb
+++ 2025/Day_09_Movie_Theater/movie_theater.rb
@@ -13,16 +13,28 @@ def calculation_info_part_one(tile_calculator)
   INFO
 end
 
+def calculation_info_part_two(tile_calculator)
+  tile_calculator.add_green_tiles
+  tile_calculator.calculate_valid_rectangles
+  <<~INFO
+    Imported #{tile_calculator.red_tiles.size} red tiles and added #{tile_calculator.green_tiles.size} green tiles.
+    The largest valid area of any rectangle using only red and green tiles is #{tile_calculator.max_valid_area}.
+
+  INFO
+end
+
 tile_calculator = TileCalculator.new
 
-puts "####### PART ONE #####################################"
+puts "####### PART ONE & TWO ###############################"
 puts "#### Importing sample data ###########################"
 
 tile_calculator.import(sample_data)
 puts calculation_info_part_one(tile_calculator)
+puts calculation_info_part_two(tile_calculator)
 
 puts "#### Importing puzzle data ###########################"
 
 tile_calculator.clean
 tile_calculator.import(puzzle_data)
 puts calculation_info_part_one(tile_calculator)
+puts calculation_info_part_two(tile_calculator)

ba7291183fd51788e99420dce7ed763b87526ec6
Björn Jensen <mirouhh@googlemail.com>
2026-01-14 16:03:58 +0100

    [Factory : Part 01] feat: it is possible to set the indicator lights of a machine

--- 2025/Day_10_Factory /lib/machine.rb
+++ 2025/Day_10_Factory /lib/machine.rb
@@ -1,6 +1,12 @@
 class Machine
 
-  def indicator_lights
-    ''
+  attr_reader :indicator_lights
+
+  def initialize
+    @indicator_lights = []
+  end
+
+  def indicator_lights=(definition)
+    @indicator_lights = definition[1...-1].chars
   end
 end

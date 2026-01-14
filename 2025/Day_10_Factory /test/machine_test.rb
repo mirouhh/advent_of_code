a93bf4ceccef55125d39c02b48889f0e226edfaf
Björn Jensen <mirouhh@googlemail.com>
2026-01-14 15:40:17 +0100

    [Factory : Part 01] test: a new machine has no indicator lights set

--- 2025/Day_10_Factory /test/machine_test.rb
+++ 2025/Day_10_Factory /test/machine_test.rb
@@ -2,4 +2,9 @@ require 'minitest/autorun'
 require_relative '../lib/machine'
 
 class MachineTest < Minitest::Test
+
+  def test_new_machine_has_no_indicator_lights
+    machine = Machine.new
+    assert_empty machine.indicator_lights
+  end
 end

cc8d971d0f8742f0aa5b931c1908b0806f3d6ffa
Björn Jensen <mirouhh@googlemail.com>
2026-01-14 19:45:35 +0100

    [Factory : Part 01] feat: one can check if a machine is running

--- 2025/Day_10_Factory /lib/machine.rb
+++ 2025/Day_10_Factory /lib/machine.rb
@@ -2,13 +2,19 @@ class Machine
 
   def initialize
     @indicator_lights = []
+    @initial_state = []
   end
 
   def indicator_lights=(definition)
     @indicator_lights = definition[1...-1].chars
+    @initial_state = ['.'] * @indicator_lights.length
   end
 
   def indicator_lights
     @indicator_lights.empty? ? '' : "[#{@indicator_lights.join}]"
+  end
+
+  def running?
+    @initial_state == @indicator_lights
   end
 end

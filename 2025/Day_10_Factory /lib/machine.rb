d8b1a18b3d346b8fbcc1bdf9af3b2b618fb0f588
Björn Jensen <mirouhh@googlemail.com>
2026-01-14 16:30:41 +0100

    [Factory : Part 01] feat: the indicator lights of a machine are set correctly

--- 2025/Day_10_Factory /lib/machine.rb
+++ 2025/Day_10_Factory /lib/machine.rb
@@ -1,12 +1,14 @@
 class Machine
 
-  attr_reader :indicator_lights
-
   def initialize
     @indicator_lights = []
   end
 
   def indicator_lights=(definition)
     @indicator_lights = definition[1...-1].chars
+  end
+
+  def indicator_lights
+    @indicator_lights.empty? ? '' : "[#{@indicator_lights.join}]"
   end
 end

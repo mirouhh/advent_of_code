5471a5cc0a30006acfa12b8549d6342346384f44
Björn Jensen <mirouhh@googlemail.com>
2026-01-14 16:05:26 +0100

    [Factory : Part 01] refactor: remove redundant code to create a new machine

--- 2025/Day_10_Factory /test/machine_test.rb
+++ 2025/Day_10_Factory /test/machine_test.rb
@@ -3,14 +3,16 @@ require_relative '../lib/machine'
 
 class MachineTest < Minitest::Test
 
+  def setup
+    @machine = Machine.new
+  end
+
   def test_new_machine_has_no_indicator_lights
-    machine = Machine.new
-    assert_empty machine.indicator_lights
+    assert_empty @machine.indicator_lights
   end
 
   def test_indicator_lights_can_be_set
-    machine = Machine.new()
-    machine.indicator_lights = '[.##.]'
-    refute_empty machine.indicator_lights
+    @machine.indicator_lights = '[.##.]'
+    refute_empty @machine.indicator_lights
   end
 end

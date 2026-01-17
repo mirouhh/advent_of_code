7a5a3be005e3ce6df37e232421264bc9fd31afc3
Björn Jensen <mirouhh@googlemail.com>
2026-01-17 13:31:19 +0100

    [Factory : Part 01] refactor: improving readability of test code by making some things already available when a test starts

--- 2025/Day_10_Factory /test/machine_test.rb
+++ 2025/Day_10_Factory /test/machine_test.rb
@@ -5,6 +5,10 @@ class MachineTest < Minitest::Test
 
   def setup
     @machine = Machine.new
+    @indicator_lights = '[.##.]'
+    @initial_state = '....'
+    @button_wiring_schematics = '(3) (1,3) (2) (2,3) (0,2) (0,1)'
+    @joltage_requirements = '{3,5,4,7}'
   end
 
   def test_new_machine_has_no_indicator_lights
@@ -12,24 +16,22 @@ class MachineTest < Minitest::Test
   end
 
   def test_indicator_lights_can_be_set
-    @machine.indicator_lights = '[.##.]'
+    @machine.indicator_lights = @indicator_lights
     refute_empty @machine.indicator_lights
   end
 
   def test_indicator_lights_are_set_correctly
-    indicator_lights = '[.##.]'
-    @machine.indicator_lights = indicator_lights
-    assert_equal indicator_lights, @machine.indicator_lights
+    @machine.indicator_lights = @indicator_lights
+    assert_equal @indicator_lights, @machine.indicator_lights
   end
 
   def test_machine_is_not_running
-    indicator_lights = '[.##.]'
-    @machine.indicator_lights = indicator_lights
+    @machine.indicator_lights = @indicator_lights
     refute @machine.running?
   end
 
   def test_machine_is_running
-    @machine.indicator_lights = '....'
+    @machine.indicator_lights = @initial_state
     assert @machine.running?
   end
 
@@ -38,18 +40,17 @@ class MachineTest < Minitest::Test
   end
 
   def test_button_wiring_schematics_can_be_set
-    @machine.button_wiring_schematics = '(3) (1,3) (2) (2,3) (0,2) (0,1)'
+    @machine.button_wiring_schematics = @button_wiring_schematics
     refute_empty @machine.button_wiring_schematics
   end
 
   def test_button_wiring_schematics_is_set_correctly
-    button_wiring_schematics = '(3) (1,3) (2) (2,3) (0,2) (0,1)'
-    @machine.button_wiring_schematics = button_wiring_schematics
-    assert_equal button_wiring_schematics, @machine.button_wiring_schematics
+    @machine.button_wiring_schematics = @button_wiring_schematics
+    assert_equal @button_wiring_schematics, @machine.button_wiring_schematics
   end
 
   def test_toggling_works
-    @machine.current_state = '....'
+    @machine.current_state = @initial_state
     @machine.toggle('2')
     assert_equal '..#.', @machine.current_state
   end
@@ -59,9 +60,9 @@ class MachineTest < Minitest::Test
   end
 
   def test_machine_is_ready
-    @machine.indicator_lights = '[.##.]'
-    @machine.button_wiring_schematics = '(3) (1,3) (2) (2,3) (0,2) (0,1)'
-    @machine.joltage_requirements = '{3,5,4,7}'
+    @machine.indicator_lights = @indicator_lights
+    @machine.button_wiring_schematics = @button_wiring_schematics
+    @machine.joltage_requirements = @joltage_requirements
     assert @machine.ready?
   end
 end

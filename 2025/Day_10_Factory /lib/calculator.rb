26fabced853dc5ac8814d5d991e1ced1cb8ad155
Björn Jensen <mirouhh@googlemail.com>
2026-01-17 15:00:50 +0100

    [Factory : Part 01] feat: a new calculator is empty

---
+++ 2025/Day_10_Factory /lib/calculator.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class Calculator
+
+  def empty?
+    true
+  end
+
+end
--- 2025/Day_10_Factory /test/calulator_test.rb
+++ 2025/Day_10_Factory /test/calulator_test.rb
@@ -1,4 +1,5 @@
 require 'minitest/autorun'
+require_relative '../lib/calculator'
 
 class CalulatorTest < Minitest::Test
 

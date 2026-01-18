1af005cb1d29d251f3f3668fbc24192a831593c5
Björn Jensen <mirouhh@googlemail.com>
2026-01-18 10:06:39 +0100

    [Factory : Part 01] test: a calculator should be able to calculate the fewest button presses for a machine

--- 2025/Day_10_Factory /test/calulator_test.rb
+++ 2025/Day_10_Factory /test/calulator_test.rb
@@ -19,4 +19,13 @@ class CalculatorTest < Minitest::Test
     refute_empty calculator
   end
 
+  def test_calculator_can_calculate_fewest_button_presses
+    machine = Machine.new()
+    machine.indicator_lights = '[.##.]'
+    machine.button_wiring_schematics = '(3) (1,3) (2) (2,3) (0,2) (0,1)'
+    machine.joltage_requirements = '{3,5,4,7}'
+    calculator = Calculator.new
+    assert_equal 2, calculator.fewest_button_presses(machine)
+  end
+
 end

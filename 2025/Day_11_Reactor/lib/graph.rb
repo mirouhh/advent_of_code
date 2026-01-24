fee93608c317e0d91cbf368079a8f20f1c85a6e9
Björn Jensen <mirouhh@googlemail.com>
2026-01-24 21:00:48 +0100

    [Reactor : Part 01] feat: one can add a vertex to an existing graph

--- 2025/Day_11_Reactor/lib/graph.rb
+++ 2025/Day_11_Reactor/lib/graph.rb
@@ -1,7 +1,16 @@
 class Graph
 
-  def empty?
-    true
+  attr_reader :vertices
+
+  def initialize
+    @vertices = []
   end
 
+  def empty?
+    @vertices.empty?
+  end
+
+  def add_vertex(vertex)
+    @vertices << vertex
+  end
 end

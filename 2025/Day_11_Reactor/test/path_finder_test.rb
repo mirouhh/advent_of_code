9bd1ae8e81355d43f2ff76798b7f3b07c8cd4cc5
Björn Jensen <mirouhh@googlemail.com>
2026-01-25 13:41:05 +0100

    [Reactor : Part 01] test: it should be possible to add a graph to a path finder

--- 2025/Day_11_Reactor/test/path_finder_test.rb
+++ 2025/Day_11_Reactor/test/path_finder_test.rb
@@ -8,4 +8,14 @@ class PathFinderTest < Minitest::Test
     assert path_finder.empty?
   end
 
+  def test_add_graph
+    path_finder = PathFinder.new
+    graph = Graph.new
+    graph.add_vertex('aaa')
+    graph.add_vertex('bbb')
+    graph.add_vertex('ccc')
+    path_finder.add(graph)
+    refute path_finder.empty?
+  end
+
 end

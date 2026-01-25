c270c3f478b802401bfc244d46c3cd49a8d50dd9
Björn Jensen <mirouhh@googlemail.com>
2026-01-25 12:45:53 +0100

    [Reactor : Part 01] test: it should be possible to add vertices to a path

--- 2025/Day_11_Reactor/test/path_test.rb
+++ 2025/Day_11_Reactor/test/path_test.rb
@@ -3,9 +3,15 @@ require_relative '../lib/path'
 
 class PathTest < Minitest::Test
 
-  def test_path_is_empty
+  def test_is_empty
     path = Path.new
     assert path.empty?
+  end
+
+  def test_add_vertex
+    path = Path.new
+    path.add('aaa')
+    refute path.empty?
   end
 
 end

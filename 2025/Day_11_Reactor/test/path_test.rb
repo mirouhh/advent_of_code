4c289b29082b30a687ce330d61918d6036fc83eb
Björn Jensen <mirouhh@googlemail.com>
2026-01-25 12:53:41 +0100

    [Reactor : Part 01] refactor: improving readability of path tests by removing duplicate code

--- 2025/Day_11_Reactor/test/path_test.rb
+++ 2025/Day_11_Reactor/test/path_test.rb
@@ -3,22 +3,23 @@ require_relative '../lib/path'
 
 class PathTest < Minitest::Test
 
+  def setup
+    @path = Path.new
+  end
+
   def test_is_empty
-    path = Path.new
-    assert path.empty?
+    assert @path.empty?
   end
 
   def test_add_vertex
-    path = Path.new
-    path.add('aaa')
-    refute path.empty?
+    @path.add('aaa')
+    refute @path.empty?
   end
 
   def test_string_representation
-    path = Path.new
-    path.add('aaa')
-    path.add('bbb')
-    assert_equal 'aaa -> bbb', path.to_s
+    @path.add('aaa')
+    @path.add('bbb')
+    assert_equal 'aaa -> bbb', @path.to_s
   end
 
 end

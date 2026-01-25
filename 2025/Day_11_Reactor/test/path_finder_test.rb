311165f57c77aafb242c748b758c7275c85a531a
Björn Jensen <mirouhh@googlemail.com>
2026-01-25 13:51:43 +0100

    [Reactor : Part 01] test: it should be possible to find paths from one node to anotherr

--- 2025/Day_11_Reactor/test/path_finder_test.rb
+++ 2025/Day_11_Reactor/test/path_finder_test.rb
@@ -18,4 +18,20 @@ class PathFinderTest < Minitest::Test
     refute path_finder.empty?
   end
 
+  def test_find_paths
+    path_finder = PathFinder.new
+    graph = Graph.new
+    graph.add_vertex('aaa')
+    graph.add_vertex('bbb')
+    graph.add_vertex('ccc')
+    graph.add_vertex('ddd')
+    graph.add_edge('aaa', 'bbb')
+    graph.add_edge('aaa', 'ccc')
+    graph.add_edge('ccc', 'ddd')
+    path_finder.add(graph)
+    paths = path_finder.find_paths('aaa', 'ddd')
+    assert_equal 1, paths.count
+    assert_equal "aaa -> ccc -> ddd", paths[0].to_s
+  end
+
 end

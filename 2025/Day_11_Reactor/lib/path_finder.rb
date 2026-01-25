00347beba042164430bd1462588282d9243924c7
Björn Jensen <mirouhh@googlemail.com>
2026-01-25 14:53:13 +0100

    [Reactor : Part 01] feat: one can find paths from one node to another using dfs

--- 2025/Day_11_Reactor/lib/path_finder.rb
+++ 2025/Day_11_Reactor/lib/path_finder.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require_relative 'graph'
+require_relative 'path'
 
 class PathFinder
 
@@ -14,4 +15,29 @@ class PathFinder
   def add(graph)
     @graph = graph
   end
+
+  def find_paths(start_vertex, target_vertex)
+    @paths = []
+    path = Path.new([start_vertex])
+    dfs(path, target_vertex)
+    @paths
+  end
+
+  private
+
+  def dfs(path, target)
+    if path.last == target
+      @paths << Path.new(path.vertices)
+      return
+    end
+
+    @graph.adjacent_vertices(path.last)&.each do |neighbour|
+      next if path.include?(neighbour)
+
+      path.add(neighbour)
+      dfs(path, target)
+      path.remove
+    end
+  end
+
 end

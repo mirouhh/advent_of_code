f1ea914cd7aed090baf71517ac35600933cbdb38
Björn Jensen <mirouhh@googlemail.com>
2026-02-06 22:08:35 +0100

    [Christmas Tree Farm : Part 01] feat: one can set the quantities of a region

--- 2025/Day_12_Christmas_Tree_Farm/lib/region.rb
+++ 2025/Day_12_Christmas_Tree_Farm/lib/region.rb
@@ -3,22 +3,20 @@
 class Region
 
   attr_accessor :width, :length
+  attr_reader :quantities
 
-  def initialize(width = 0, length = 0)
+  def initialize(width = 0, length = 0, quantities = [])
     @width = width
     @length = length
+    @quantities = quantities
   end
 
   def empty?
-    @width == 0 && @length == 0
+    @width == 0 && @length == 0 && @quantities.empty?
   end
 
   def area
     width * length
-  end
-
-  def quantities
-    []
   end
 
 end

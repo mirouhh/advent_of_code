e9dbca6475647f32519a20cf2fd2fc3067276f59
Björn Jensen <mirouhh@googlemail.com>
2026-02-08 20:00:14 +0100

    [Christmas Tree Farm : Part 01] test: a new present calculator should be empty

--- 2025/Day_12_Christmas_Tree_Farm/test/present_calculator_test.rb
+++ 2025/Day_12_Christmas_Tree_Farm/test/present_calculator_test.rb
@@ -5,4 +5,9 @@ require_relative '../lib/present_calculator'
 
 class PresentCalculatorTest < Minitest::Test
 
+  def test_new_calculator_is_empty
+    present_calculator = PresentCalculator.new
+    assert present_calculator.empty?
+  end
+
 end

af3aa04eabe84bb01e0ede8d495177ebf355d23d
Björn Jensen <mirouhh@googlemail.com>
2026-02-12 20:00:12 +0100

    [Christmas Tree Farm : Part 01] feat: one can import data from a file to a calculator

--- 2025/Day_12_Christmas_Tree_Farm/lib/present_calculator.rb
+++ 2025/Day_12_Christmas_Tree_Farm/lib/present_calculator.rb
@@ -4,13 +4,45 @@ class PresentCalculator
 
   def initialize()
     @presents = []
+    @regions = []
   end
 
   def empty?
-    @presents.empty?
+    @presents.empty? && @regions.empty?
   end
 
   def add_present(present)
     @presents << present
   end
+
+  def add_region(region)
+    @regions << region
+  end
+
+  def import(file)
+    data = []
+    File.readlines("#{file}", chomp: true).each do |line |
+      if line.empty?
+        process(data)
+        data.clear
+      else
+        data << line
+      end
+    end
+  end
+
+  private
+
+  def process(data)
+    data[0].include?("x") ? process_region(data) : process_present(data)
+  end
+
+  def process_region(region_data)
+    @regions << data
+  end
+
+  def process_present(present_data)
+    @presents << data
+  end
+
 end
